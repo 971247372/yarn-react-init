3db350e5b7e70d522f36979ae35e985028688358
daisy <daisy@daisydeMacBook-Pro.local>
2018-04-17 11:07:03 +0800

    add img,fonts

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 import {Layout,Spin,Row,Col,DatePicker} from 'antd';
 import style from './style.less';
+var img=require('./image/img.png');
 class App extends Component{
     constructor(props){
         super(props);
@@ -29,6 +30,7 @@ class App extends Component{
                     <Content className={style['content']}>
                         1<br/>
                         1<br/>  
+                        <img src={img}/>
                         1<br/>  
                         1<br/>  
                         1<br/>  
