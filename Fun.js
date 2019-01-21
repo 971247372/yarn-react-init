adcc187fb2cc3cbbcbfc242407efbd9203e387a6
qingwan_zeng <qingwan_zeng@fengzhiding.com>
2019-01-21 09:14:29 +0800

    新增将base64成图片下载

--- Fun.js
+++ Fun.js
@@ -7,3 +7,15 @@ function templateFunc(obj, data) {
   });
   return JSON.parse(computed);
 }
+
+export default canvas => {
+  // const imgUri = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream'); // 获取生成的图片的url
+  // window.open(imgUri);
+  const url = canvas.toDataURL('image/png');
+  const a = document.createElement('a');
+  const event = new MouseEvent('click');
+  // 下载的图片名称
+  a.download = new Date().getTime();
+  a.href = url;
+  a.dispatchEvent(event);
+};
